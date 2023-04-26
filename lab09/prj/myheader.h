00d97a7c91ceb075043f8cb86bfcf278deffd4fa
Artem <artemruban762@gmail.com>
2023-04-26 23:07:53 +0300

    Add H file

---
+++ lab09/prj/myheader.h
@@ -0,0 +1,8 @@
+#ifndef MYHEADER_H_INCLUDED
+#define MYHEADER_H_INCLUDED
+
+void s_calculation(void);
+void wind_speed(void);
+void temperature(void);
+void number(void);
+#endif // MYHEADER_H_INCLUDED
