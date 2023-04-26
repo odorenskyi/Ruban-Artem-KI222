b353cd395c893b2f44f0e696520f472774f43cfa
Artem <artemruban762@gmail.com>
2023-04-26 23:02:45 +0300

    Static Library

--- lab09/prj/myheader.h
+++
@@ -1,8 +0,0 @@
-#ifndef MYHEADER_H_INCLUDED
-#define MYHEADER_H_INCLUDED
-
-void s_calculation(void);
-void wind_speed(void);
-void temperature(void);
-void number(void);
-#endif // MYHEADER_H_INCLUDED
